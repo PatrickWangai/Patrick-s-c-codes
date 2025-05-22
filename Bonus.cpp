6dbb91c3e9af1d1e99ced6afd8ef2d72b4e8dfb4
PatrickWangai <gm70291691@gmail.com>
2025-05-22 13:56:54 +0300

    Add files via upload

---
+++ Bonus.cpp
@@ -0,0 +1,29 @@
+/*Name:Patrick Wangai
+Admission Number:BSE-01-0052/2025
+Description:Determining bonus earned depending on salary and years of service*/
+#include <iostream>
+using namespace std;
+
+int main() {
+    float salary, years_of_service, bonus, total_salary;
+
+    cout << "Enter your salary and years of service" << endl;
+    cin >> salary >> years_of_service;
+
+    if (years_of_service > 10) {
+        bonus = 0.1 * salary;
+    }
+    else if (years_of_service >= 6 && years_of_service <= 10) {
+        bonus = 0.08 * salary;
+    }
+    else {
+        bonus = 0.06 * salary;
+    }
+
+    total_salary = bonus + salary;
+
+    cout << "The Net Bonus: " << bonus << endl;
+    cout << "The total salary is: " << total_salary << endl;
+
+    return 0;
+}
